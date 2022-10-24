e1e37fea2e59df74e391026ba19ddeaa1f829b00
alexperezortuno <alexperezortuno@gmail.com>
2022-10-24 15:38:00 -0300

    add log

--- internal/platform/storage/store/store.go
+++ internal/platform/storage/store/store.go
@@ -1,22 +1,23 @@
 package store
 
 import (
-    "context"
-    "fmt"
-    "github.com/go-redis/redis/v9"
-    "go-url-shortner/tools/environment"
-    "time"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v9"
+	"go-url-shortner/tools/environment"
+	"log"
+	"time"
 )
 
 // StorageService is struct wrapper around raw Redis client
 type StorageService struct {
-    redisClient *redis.Client
+	redisClient *redis.Client
 }
 
 // Top level declarations for the storeService and Redis context
 var (
-    storeService = &StorageService{}
-    ctx          = context.Background()
+	storeService = &StorageService{}
+	ctx          = context.Background()
 )
 
 var params = environment.Server()
@@ -25,35 +26,35 @@ const CacheDuration = 6 * time.Hour
 
 // InitializeStore is initializing the store service and return a store pointer
 func InitializeStore() *StorageService {
-    rdb := redis.NewClient(&redis.Options{
-        Addr:     params.RedisHost,
-        Password: params.RedisPass, // no password set
-        DB:       params.RedisDb,   // use default DB
-    })
-    
-    pong, err := rdb.Ping(ctx).Result()
-    if err != nil {
-        panic(fmt.Sprintf("Error init Redis: %v", err))
-    }
-    
-    fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
-    storeService.redisClient = rdb
-    return storeService
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     params.RedisHost,
+		Password: params.RedisPass, // no password set
+		DB:       params.RedisDb,   // use default DB
+	})
+
+	pong, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Println(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	log.Println(fmt.Sprintf("Redis started successfully: pong message = {%s}", pong))
+	storeService.redisClient = rdb
+	return storeService
 }
 
 func SaveURLInRedis(shortURL, originalURL string) {
-    err := storeService.redisClient.Set(ctx, shortURL, originalURL, CacheDuration).Err()
-    if err != nil {
-        panic(fmt.Sprintf("Failed SaveURLInRedis | Error: %v - shortURL: %s - originalURL: %s\n",
-            err, shortURL, originalURL))
-    }
+	err := storeService.redisClient.Set(ctx, shortURL, originalURL, CacheDuration).Err()
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed SaveURLInRedis | Error: %v - shortURL: %s - originalURL: %s\n",
+			err, shortURL, originalURL))
+	}
 }
 
 func RetrieveInitialURLFromRedis(shortURL string) string {
-    result, err := storeService.redisClient.Get(ctx, shortURL).Result()
-    if err != nil {
-        panic(fmt.Sprintf("Failed RetrieveInitialURLFromRedis | Error: %v - shortURL: %s\n",
-            err, shortURL))
-    }
-    return result
+	result, err := storeService.redisClient.Get(ctx, shortURL).Result()
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed RetrieveInitialURLFromRedis | Error: %v - shortURL: %s\n",
+			err, shortURL))
+	}
+	return result
 }
